a832b0ccae0c704bf409d4eb31606e7930541a9a
Kevin Quigley <kquigley27@gmail.com>
2023-12-03 22:33:57 -0500

    enable send on enter

--- chat/chat.js
+++ chat/chat.js
@@ -46,3 +46,17 @@ async function generateText(prompt) {
         document.getElementById("AIResponse").innerHTML = "Error: " + error;
     }
 }
+
+// Get the input field
+var input = document.getElementById("userInput");
+
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("sendButton").click();
+  }
+});
